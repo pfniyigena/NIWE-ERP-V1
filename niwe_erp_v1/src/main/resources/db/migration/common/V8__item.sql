7a9158f05f632edb9194cc3056566fe49468b69d
pfniyigena <pfniyigena@gmail.com>
2026-05-05 14:06:09 +0200

    barcode not edit

--- niwe_erp_v1/src/main/resources/db/migration/common/V8__item.sql
+++ niwe_erp_v1/src/main/resources/db/migration/common/V8__item.sql
@@ -1,1 +1,3 @@
 CREATE UNIQUE INDEX idx_unique_item_name_clean ON core_item (LOWER(TRIM(item_name)));
+ALTER TABLE core_item ADD CONSTRAINT unique_item_name UNIQUE (item_name);
+CREATE UNIQUE INDEX unique_item_name_active ON core_item(item_name) WHERE deleted = false;
